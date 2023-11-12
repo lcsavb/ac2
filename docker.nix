0998db5e0c45d24dc2d96e1a848c9b03f96cb362
Lucas Barros <lcsavb@gmail.com>
2023-11-13 00:07:16 +0100

    Struggling to make a docker image with nix

---
+++ docker.nix
@@ -0,0 +1,50 @@
+{ pkgs ? import <nixpkgs> {} 
+, pkgsLinux ? import <nixpkgs> { system = "x86_64-linux"; }
+}:
+
+
+let
+    currentDir = ./.;
+    
+    appFolder = pkgs.copyPath {
+      src = currentDir;
+      name = "ac2";
+    };
+
+    nixFromDockerHub = pkgs.dockerTools.pullImage {
+    imageName = "nixos/nix";
+    imageDigest = "sha256:85299d86263a3059cf19f419f9d286cc9f06d3c13146a8ebbb21b3437f598357";
+    sha256 = "19fw0n3wmddahzr20mhdqv6jkjn1kanh6n2mrr08ai53dr8ph5n7";
+    finalImageTag = "2.2.1";
+    finalImageName = "nix";
+  };
+    pypdftk = pkgs.python3Packages.buildPythonPackage rec {
+    pname = "pypdftk";
+    version = "0.5";
+    src = pkgs.python3Packages.fetchPypi {
+      inherit pname version;
+      sha256 = "sha256-tvfwABM0gIQrO/n4Iiy2vV4rIM9QxewFDy5E2ffcMpY";
+
+    };
+    doCheck = false;
+  };
+in
+pkgs.dockerTools.buildImage {
+  name = "ac2";
+  tag = "dev";
+  fromImage = nixFromDockerHub;
+  contents = [ 
+	pkgs.python3
+	pkgs.python3Packages.django
+	pkgs.pdftk
+	pypdftk
+];
+  config = {
+    Cmd = [ "${pkgs.python3}/bin/python3" "${appFolder}/manage.py" "runserver" "127.0.0:8000" ];
+    ExposedPorts = {
+      "8000/tcp" = {};
+    };
+  };
+
+
+}
