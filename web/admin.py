878a2a225c0b5ca775cb8ddd737c0b6addbe5bf5
Lucas Barros <lcsavb@gmail.com>
2023-11-15 18:51:00 +0100

    Basic database model done and classes registered in the admin

--- web/admin.py
+++ web/admin.py
@@ -1,3 +1,48 @@
 from django.contrib import admin
+from .models import Clinic, Disease, Doctor, Patient, Medication, Prescription, Protocol, PatientCareLink, Issuer, Visit
+
 
 # Register your models here.
+
+class ClinicAdmin(admin.ModelAdmin):
+    pass
+
+class DiseaseAdmin(admin.ModelAdmin):
+    pass
+
+class DoctorAdmin(admin.ModelAdmin):
+    pass
+
+class PatientAdmin(admin.ModelAdmin):
+    pass
+
+class MedicationAdmin(admin.ModelAdmin):
+    pass
+
+class PrescriptionAdmin(admin.ModelAdmin):  
+    pass
+
+class ProtocolAdmin(admin.ModelAdmin):  
+    pass
+
+class PatientCareLinkAdmin(admin.ModelAdmin):   
+    pass
+
+class IssuerAdmin(admin.ModelAdmin):   
+    pass
+
+class VisitAdmin(admin.ModelAdmin):   
+    pass
+
+
+admin.site.register(Clinic, ClinicAdmin)
+admin.site.register(Disease, DiseaseAdmin)
+admin.site.register(Doctor, DoctorAdmin)
+admin.site.register(Patient, PatientAdmin)
+admin.site.register(Medication, MedicationAdmin)
+admin.site.register(Prescription, PrescriptionAdmin)
+admin.site.register(Protocol, ProtocolAdmin)
+admin.site.register(PatientCareLink, PatientCareLinkAdmin)
+admin.site.register(Issuer, IssuerAdmin)
+admin.site.register(Visit, VisitAdmin)
+
--- web/models.py
+++ web/models.py
@@ -2,12 +2,49 @@ from django.db import models
 from django.conf import settings
 from django.utils.translation import gettext_lazy as _
 from django.utils import timezone
-from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager, Group, Permission
 from .managers import CustomUserManager
 from django.db import models
 
 JSONField = models.JSONField
 BooleanField = models.BooleanField
+
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(_('email address'), unique=True)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    date_joined = models.DateTimeField(default=timezone.now)
+
+    USERNAME_FIELD = 'email'
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.email
+
+    groups = models.ManyToManyField(
+        Group,
+        verbose_name=_('groups'),
+        blank=True,
+        help_text=_('The groups this user belongs to. A user will get all permissions granted to each of their groups.'),
+        related_name="customuser_groups",
+        related_query_name="customuser",
+    )
+    user_permissions = models.ManyToManyField(
+        Permission,
+        verbose_name=_('user permissions'),
+        blank=True,
+        help_text=_('Specific permissions for this user.'),
+        related_name="customuser_user_permissions",
+        related_query_name="customuser",
+    )
+
+    def __str__(self):
+        return self.email
+
+    class Meta:
+        verbose_name = _('custom user')
+        verbose_name_plural = _('custom users')
 
 class Clinic(models.Model):
     name = models.CharField(max_length=100)
@@ -21,9 +58,6 @@ class Clinic(models.Model):
     phone = models.CharField(max_length=100)
     doctors = models.ManyToManyField('Doctor', through='Issuer')
     patients = models.ManyToManyField('Patient')
-
-
-#
 
 class Disease(models.Model):
     icd = models.CharField(max_length=6, unique=True)
