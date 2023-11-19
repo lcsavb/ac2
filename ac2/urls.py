f1ec9dc5d18f0ab2447703cc73a87ed7700dbfc6
Lucas Barros <lcsavb@gmail.com>
2023-11-19 20:23:42 +0100

    Base template

--- ac2/urls.py
+++ ac2/urls.py
@@ -22,4 +22,5 @@ from users import views as user_views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', include('users.urls')),
+    path('', include('web.urls')),
     ]
--- web/views.py
+++ web/views.py
@@ -1,16 +1,9 @@
 from django.shortcuts import render
-from forms import CustomUserCreationForm
-from models import CustomUser
+from django.http import HttpResponse
 
-class SignUp(CustomUserCreationForm):
-    clinic_sus_number = forms.CharField(max_length=100)
-    clinic_name = forms.CharField(max_length=100)
-    clinic_address = forms.CharField(max_length=100)
-    clinic_address_number = forms.CharField(max_length=100)
-    clinic_phone = forms.CharField(max_length=100)
-    email = forms.EmailField(max_length=100)
+def home(request):
+    return render (request, 'web/home.html')
 
-    class Meta(CustomUserCreationForm.Meta):
-        model = CustomUser 
-        fields = ['clinic_sus_number', 'clinic_name', 'clinic_address', 'clinic_address_number', 'clinic_phone', 'email', 'password1', 'password2']
-    
+def new_prescription(request):
+    return render (request, 'web/new_prescription.html')
+
