4b8cc4744a3bdc9c195fd527c1be62c4da392321
Lucas Barros <lcsavb@gmail.com>
2023-11-23 21:28:10 +0100

    Proto registration form

--- ac2/settings.py
+++ ac2/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'web.apps.WebConfig',
     'users.apps.UsersConfig',
+    'crispy_forms',
+    'crispy_bootstrap5',
 ]
 
 MIDDLEWARE = [
@@ -118,6 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap5'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- users/views.py
+++ users/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
-from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import redirect
+from django.contrib import messages
+from .forms import UserRegisterForm
 
 def sign_up(request):
-    form = UserCreationForm()
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data["username"]
+            messages.success(request, f'Conta criada: {username}!')
+            return redirect('web-home')
+    else:           
+        form = UserRegisterForm()
     return render(request, 'users/sign_up.html', {'form': form})
